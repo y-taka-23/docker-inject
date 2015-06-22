1c9a1257cb84a9422969602217640ae61a9fff55
TAKAHASHI Yuto <ytaka23dev@gmail.com>
2015-06-23 02:38:04 +0900

    Implement core procedures for injection

--- injector.go
+++ injector.go
@@ -1,18 +1,59 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 )
 
-type Injector struct {
+type injector struct {
 }
 
-func newInjector() *Injector {
-	return &Injector{}
+func newInjector() *injector {
+	return &injector{}
 }
 
-func (inj *Injector) run(args []string) {
-	for i, a := range args {
-		fmt.Printf("%d: %s\n", i, a)
+func (inj *injector) run(args []string) error {
+	if len(args) != 2 {
+		errors.New("two arguments required")
 	}
+	srcPath := args[0]
+	container, tgtPath, err := inj.parseTarget(args[1])
+	if err != nil {
+		return err
+	}
+	fmt.Println("%s ==> &s:&s", srcPath, container, tgtPath)
+	return nil
+}
+
+func (inj *injector) parseTarget(tgt string) (string, string, error) {
+	strs := strings.Split(tgt, ":")
+	if len(strs) != 2 {
+		return "", "", fmt.Errorf("invalid target: %s", tgt)
+	}
+	return strs[0], strs[1], nil
+}
+
+func (inj *injector) inject(src, con, tgt string) error {
+	// TODO revise with cmd.Stdin
+	cmd := exec.Command(
+		"docker", "exec", "-it", con,
+		"/bin/bash", "-c", "'cat > "+tgt+"'", "<", src,
+	)
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (inj *injector) mkdir(con, tgt string) error {
+	cmd := exec.Command("docker", "exec", "-it", con, "mkdir", "-p", tgt)
+	cmd.Stderr = os.Stderr
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	return nil
 }
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/codegangsta/cli"
 	"os"
 )
@@ -11,7 +12,12 @@ func main() {
 	app.Usage = "Copy files/directories from hosts to running Docker containers"
 	app.Version = "0.0.0"
 	app.Action = func(c *cli.Context) {
-		newInjector().run(c.Args())
+		inj := newInjector()
+		if err := inj.run(c.Args()); err != nil {
+			fmt.Fprintf(os.Stderr, "%s: %s\n", app.Name, err)
+			cli.ShowAppHelp(c)
+			os.Exit(1)
+		}
 	}
 	app.Run(os.Args)
 }
